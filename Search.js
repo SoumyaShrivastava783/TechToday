241bc3fc0f44316a04bb7bd1998fe0b1b4ed2536
SoumyaShrivastava783 <somyashrivastava66@gmail.com>
2024-05-19 16:08:22 +0530

    Add files via upload

---
+++ Search.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+const Search = () => {
+  const { query, searchPost } = useGlobalContext();
+  return (
+    <>
+      <h1>TECH NEWS WEBISITE</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <input
+            type="text"
+            placeholder="search here"
+            value={query}
+            onChange={(e) => searchPost(e.target.value)}
+          />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Search;
+
+
+
